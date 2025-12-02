520a3beaf767606c525a5b8b8522c7770cc0edcf
Kholoud Mohamed <kholoud.mohamed+bwag@knab.nl>
2025-12-02 14:41:19 +0100

    Add config files

---
+++ src/main/java/framework/Config/ConfigManager.java
@@ -0,0 +1,5 @@
+package framework.Config;
+
+public class ConfigManager {
+    
+}
---
+++ src/main/java/framework/Config/TestConfig.java
@@ -0,0 +1,30 @@
+package framework.Config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.LoadType;
+import org.aeonbits.owner.Config.Sources;
+
+@LoadPolicy(LoadType.MERGE)
+@Sources({
+    "system:properties",
+    "system:env",
+    "file:src/test/config/${test.environment}.properties",
+    "file:src/test/config/default.properties"
+})
+public interface TestConfig extends Config {
+    
+    @Key("base.url")
+    String baseUrl();
+
+    @Key("test.environment")
+    @DefaultValue("prod")
+    String testEnvironment();
+    
+    @Key("request.timeout")
+    int requestTimeout();
+    
+    @Key("connection.timeout")
+    int connectionTimeout();
+}
+
